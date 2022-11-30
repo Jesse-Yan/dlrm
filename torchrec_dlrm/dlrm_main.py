5014e37a6c4ea63e8b3ad224e932f5f0a7f62b0d
saurabh <agarwal@cs.wisc.edu>
2022-11-29 23:05:01 -0600

    Back to using custom plan

--- torchrec_dlrm/dlrm_main.py
+++ torchrec_dlrm/dlrm_main.py
@@ -745,18 +745,18 @@ def main(argv: List[str]) -> None:
     print("Rank {}".format(dist.get_rank()))
     print("Plan {}".format(shard_plan))
     # planner = EmbeddingShardingPlanner(topology=topology)
-    # model = DistributedModelParallel(
-    # module=train_model,
-    # env=ShardingEnv.from_process_group(dist.group.WORLD),
-    # device=device,
-    # sharders=sharders,
-    # plan=shard_plan,
-    # )
     model = DistributedModelParallel(
         module=train_model,
+        env=ShardingEnv.from_process_group(dist.group.WORLD),
         device=device,
-        sharders=cast(List[ModuleSharder[nn.Module]], sharders),
+        sharders=sharders,
+        plan=shard_plan,
     )
+    # model = DistributedModelParallel(
+    # module=train_model,
+    # device=device,
+    # sharders=cast(List[ModuleSharder[nn.Module]], sharders),
+    # )
 
     print("Model plan {}".format(model._plan))
 
